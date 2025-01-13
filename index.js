88e619005a381485482353fb022a28d1b22084c3
Ammwal <bismakhan7334@gmail.com>
2025-01-13 19:31:13 +0500

    Updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images' , express.static('public/images'))
 app.use(express.urlencoded({ extended: true }));
 
 
